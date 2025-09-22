02f8939de863169beab0d942409e7071a2c11f21
gpeg <pinogallegos.gabriel@gmail.com>
2025-09-21 21:05:39 -0600

    Add Background and Navbar

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/Navbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-green-200 via-white to-yellow-200">
+      <Navbar />
+    </div>
+  );
+}
