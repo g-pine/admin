4bb6cfb0bc540f03835923cc667904599e51c06b
gpeg <pinogallegos.gabriel@gmail.com>
2025-09-23 20:13:29 -0600

    add dashboard and create database

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,23 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
+
 export default function DashboardPage() {
+    const { user } = useUser();
     return (
         <div className="container mx-auto px-4 py-8">
-            <h1 className="text-3xl font-bold mb-6">Dashboard</h1>
-            <p className="text-gray-700">We are building a big project here</p>
+            <Navbar/>
+            <main className="container mx-auto px-4 py-6 sm:py-8">
+                <div className="mb-6 sm:mb-8">
+                    <h1 className="text-2xl sm:text-3xl font-bold text-gray-900">
+                        Welcome back,  {user?.firstName || user?.username}! 
+                    </h1>
+                    <p className="text-gray-600">
+                        Here's what's happening with your admin panel today.
+                    </p>
+                </div>
+            </main>
         </div>
     );
 }
